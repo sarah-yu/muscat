b760f14334e777e57162c9d5aaeaf4ae8d5752ce
Sarah Yu <sarahyu0692@gmail.com>
2018-03-03 12:27:40 -0500

    remove react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-	<Router>
-		<MuiThemeProvider>
-			<App />
-		</MuiThemeProvider>
-	</Router>,
+	<MuiThemeProvider>
+		<App />
+	</MuiThemeProvider>,
 	document.getElementById('root')
 )
 registerServiceWorker()
