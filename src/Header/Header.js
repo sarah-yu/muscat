ad5961038b52ab6f03dada242a86c24f03771548
Sarah Yu <sarahyu0692@gmail.com>
2018-03-03 14:36:38 -0500

    fix link to home

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
-
 import './Header.css'
+
+let home
+if (document.location.hostname === 'localhost') {
+	home = '/'
+} else {
+	home = '/muscat'
+}
 
 const Header = () => (
 	<header>
 		<div className="header-wrapper">
 			<h1 className="header-title">
-				<a href="/">muscat</a>
+				<a href={home}>muscat</a>
 			</h1>
 			<p className="header-description">Find your purrfect friend</p>
 		</div>
